3f98e7bc3cd55c66860981bc5b1d7089fd6d1a9f
jazgamarra <j444min@gmail.com>
2025-03-30 02:34:48 -0300

    bootstrap 0 jamin 1

--- Empleado/forms.py
+++ Empleado/forms.py
@@ -1,8 +1,49 @@
-# empleados/forms.py
 from django import forms
-from nomina.models import Empleado
+from nomina.models import Empleado, Departamento, Contrato
 
 class EmpleadoForm(forms.ModelForm):
     class Meta:
         model = Empleado
-        fields = ["id_departamento","id_contrato",'nombres', 'apellidos', 'cedula', 'fecha_nacimiento', 'telefono', 'email', 'fecha_ingreso', 'activo']
+        fields = ['id_departamento', 'id_contrato', 'nombres', 'apellidos', 'cedula', 'fecha_nacimiento', 'telefono', 'email', 'fecha_ingreso', 'activo']
+
+        widgets = {
+            'nombres': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese los nombres'
+            }),
+            'apellidos': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese los apellidos'
+            }),
+            'cedula': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese la cédula'
+            }),
+            'fecha_nacimiento': forms.DateInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Seleccione la fecha de nacimiento',
+                'type': 'date'  # para que se vea bien
+            }),
+            'telefono': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el teléfono'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el email'
+            }),
+            'fecha_ingreso': forms.DateInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Seleccione la fecha de ingreso',
+                'type': 'date'
+            }),
+            'activo': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+            'id_departamento': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+            'id_contrato': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+        }
