f9b8f3e55cb5826740e50e6788f52dba678c75fa
jazgamarra <j444min@gmail.com>
2025-03-30 10:45:40 -0300

    empleados mayuscula a minuscula

--- nomina/urls.py
+++ nomina/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('empleados/', include('Empleado.urls')),
+    path('empleados/', include('empleado.urls')),
 
 ]
