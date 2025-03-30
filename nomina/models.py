c98c6cbddaa1c95f7098ec2e1ea03e75c689273b
jazgamarra <j444min@gmail.com>
2025-03-30 01:13:01 -0300

    minor fixes - str para que se muestre bien en el formulario

--- nomina/models.py
+++ nomina/models.py
@@ -1,10 +1,3 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 class Cargo(models.Model):
@@ -51,8 +44,11 @@ class Contrato(models.Model):
     salario_acordado = models.DecimalField(max_digits=15, decimal_places=5)
     contrato_activo = models.BooleanField()
 
+    def __str__(self):
+        salario = f"{int(self.salario_acordado)}" if self.salario_acordado else "No definido"
+        return f"{self.id_contrato} | {self.tipo_contrato} | {self.fecha_inicio} | {salario}" 
+    
     class Meta:
-        
         db_table = 'contrato'
 
 
@@ -73,8 +69,10 @@ class Departamento(models.Model):
     id_departamento = models.IntegerField(primary_key=True)
     departamento = models.CharField()
 
+    def __str__(self):
+        return self.departamento 
+
     class Meta:
-        
         db_table = 'departamento'
 
 
