b40474bfb3ec578d45faff4351ab4c99d9266edf
Aldo M <aldomedina008@gmail.com>
2025-05-01 18:22:24 -0300

    Actualizacion del modelo de concepto

--- concepto/models.py
+++ concepto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Concepto(models.Model):
+    descripcion = models.CharField(max_length=255)
+    es_fijo = models.BooleanField(default=False)
+    es_deb_cred = models.BooleanField(default=False)
+    porcentaje = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    permite_cuotas = models.BooleanField(default=False)
+    cant_cuota = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.descripcion
