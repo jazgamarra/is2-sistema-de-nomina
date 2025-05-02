dd272860b6b0f0de055d364ad7b2f3260a264c5b
Aldo M <aldomedina008@gmail.com>
2025-05-02 13:46:21 -0300

    Eliminacion de la app concepto

--- concepto/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Concepto
-
-@admin.register(Concepto)
-class ConceptoAdmin(admin.ModelAdmin):
-    list_display = ['descripcion', 'es_fijo', 'es_deb_cred', 'porcentaje', 'permite_cuotas', 'cant_cuota']
-    list_filter = ['es_fijo', 'es_deb_cred', 'permite_cuotas']  # Filtros útiles para la interfaz
-    search_fields = ['descripcion']  # Permite buscar por la descripción
--- concepto/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.listar_conceptos, name='listar_conceptos'),
-    path('crear/', views.crear_concepto, name='crear_concepto'),
-    path('editar/<int:pk>/', views.editar_concepto, name='editar_concepto'),
-    path('eliminar/<int:pk>/', views.eliminar_concepto, name='eliminar_concepto'),
-]
