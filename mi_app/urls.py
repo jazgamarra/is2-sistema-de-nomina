6e9b21dfa3de636b0b8f0dca68be3b400c58bab6
noeinsfran <beainsfran24@fpuna.edu.py>
2025-05-11 18:49:03 -0300

    Mejoras visuales y corrección de vínculos

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.menu_inicio, name='menu_inicio'),
+    path('', views.menu_inicio, name='menu'),  # <-- Cambiado aquí
 ]
+
+
